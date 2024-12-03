687e5dafec43023cb6065e0b3fa95e8af2827de2
rricher <ryanricher@gmail.com>
2024-12-02 21:31:01 -0800

    finished day 2

--- day_2/d2p2.py
+++ day_2/d2p2.py
@@ -1,80 +1,43 @@
 def main() -> None:
     safe = 0
     data = []
-    with open("./input.txt", "r") as file:
+    with open("./failing.txt", "r") as file:
         for line in file:
             numbers = []
             for item in line.split():
                 numbers.append(int(item))
             data.append(numbers)
+    # print(data[0])
+    # for i in range(len(data[0])):
+    #     level = [obj for j, obj in enumerate(data[0]) if j != i]
+    #     print(level)
     for report in data:
-        prnt = False
-        unsafe = False
-        increase = False
-        bad = False
-        prev = 0
-        level = 1
-        if (
-            (report[0] < report[1] and report[1] < report[2])
-            or (report[1] < report[2] and report[2] < report[3])
-            or (report[0] < report[2] and report[2] < report[3])
-            or (report[0] < report[1] and report[1] < report[3])
-        ):
-            increase = True
-        while level < len(report):
-            diff = 0
-            if increase:
-                diff = report[level] - report[prev]
-            else:
-                diff = report[prev] - report[level]
-            if diff < 1 or diff > 3:
-                if bad:
-                    # print("increase" if increase else "decrease")
-                    if prnt:
-                        print(report)
-                        print("unsafe", report[prev], report[level])
-                    unsafe = True
-                    break
-                # print(
-                #     "increase" if increase else "decrease",
-                #     (report[prev], report[level]),
-                #     report,
-                # )
-                # print((report[prev], report[level]))
-                # print(report)
-                bad = True
-                if prev == 0:
-                    if increase:
-                        diff = report[level + 1] - report[level]
-                    else:
-                        diff = report[level] - report[level + 1]
-                if level + 1 == len(report) - 1:
-                    print("end one", report)
-                    prnt = True
-            else:
-                prev = level
-            level += 1
-
-        #         if prev == 0:
-        #         if increase:
-        #             diff = report[level + 1] - report[level]
-        #         else:
-        #             diff = report[level] - report[level + 1]
-        #     if diff > 0 and diff < 4:
-        #         report.pop(prev)
-        #     else:
-        #         report.pop(level)
-        #     prev = 0
-        #     level = 1
-        # else:
-        #     prev = level
-        #     level += 1
-
-        if not unsafe:
-            safe += 1
-            if prnt:
-                print("safe")
+        for i in range(len(report)):
+            level = [obj for j, obj in enumerate(report) if j != i]
+            if check(level):
+                safe += 1
+                print(report)
+                break
     print(safe)
+
+
+def check(level):
+    if level[0] < level[1]:
+        inc = True
+    else:
+        inc = False
+    for i in range(len(level) - 1):
+        diff = 0
+        cur = level[i]
+        next = level[i + 1]
+        if inc:
+            diff = next - cur
+        else:
+            diff = cur - next
+        if diff < 1 or diff > 3:
+            return False
+    print(level)
+    return True
 
 
 if __name__ == "__main__":
